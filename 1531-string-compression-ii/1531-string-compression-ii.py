9887d18f688707e7e9ab3b8bda2005550db4364c
Insooooo <60099190+Insu-Park@users.noreply.github.com>
2023-12-28 16:56:02 +0900

    Time: 3020 ms (5%), Space: 371.2 MB (15%) - LeetHub

--- 1531-string-compression-ii/1531-string-compression-ii.py
+++ 1531-string-compression-ii/1531-string-compression-ii.py
@@ -1,37 +1,17 @@
 class Solution:
     def getLengthOfOptimalCompression(self, s: str, k: int) -> int:
-        n = len(s)
-        dp = [[101] * 101 for _ in range(101)] 
-
-        for i in range(n + 1):
-            for j in range(n + 1):
-                dp[i][j] = 101
-
-        dp[0][0] = 0 
-
-        for i in range(1, n + 1):
-            for j in range(k + 1):
-                count, del_count = 0, 0  # Variables to store the count of consecutive characters and deletions
-
-                for t in range(i, 0, -1):
-                    if s[t - 1] == s[i - 1]:
-                        count += 1  # Increment count if the characters are the same
-                    else:
-                        del_count += 1  # Increment deletion count if the characters are different
-
-                    if j - del_count >= 0:
-                        temp = 0
-
-                        if count >= 100:
-                            temp = 3
-                        elif count >= 10:
-                            temp = 2
-                        elif count >= 2:
-                            temp = 1
-
-                        dp[i][j] = min(dp[i][j], dp[t - 1][j - del_count] + 1 + temp)
-
-                if j > 0:
-                    dp[i][j] = min(dp[i][j], dp[i - 1][j - 1])
-
-        return dp[n][k]
+        @lru_cache(None)
+        def counter(start, last, last_count, left):
+            if left < 0:
+                return float('inf') # this is impossible
+            if start >= len(s):
+                return 0
+            if s[start] == last:
+                incr = 1 if last_count == 1 or last_count == 9 or last_count == 99 else 0
+                return incr + counter(start+1, last, last_count+1, left) 
+            else:
+                keep_counter = 1 + counter(start+1, s[start], 1, left)
+                del_counter =  counter(start + 1, last, last_count, left - 1)
+            return min(keep_counter, del_counter)
+            
+        return counter(0, "", 0, k)
