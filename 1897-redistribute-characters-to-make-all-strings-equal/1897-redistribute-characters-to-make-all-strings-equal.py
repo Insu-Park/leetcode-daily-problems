79143a057e5780d27d6a02aad274aa8da6f329fa
Insooooo <60099190+Insu-Park@users.noreply.github.com>
2023-12-30 21:42:24 +0900

    Time: 73 ms (26.98%), Space: 17.5 MB (9.52%) - LeetHub

--- 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.py
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.py
@@ -1,11 +1,9 @@
 class Solution:
     def makeEqual(self, words: List[str]) -> bool:
-        counters = {}
-        for word in words:
-            counters = dict(Counter(counters) + Counter(word))
-        
-        for item in counters.values():
-            if item % len(words) != 0:
+
+        word = "".join(words)
+        for it in Counter(word).values():
+            if it % len(words) != 0:
                 return False
 
         return True
