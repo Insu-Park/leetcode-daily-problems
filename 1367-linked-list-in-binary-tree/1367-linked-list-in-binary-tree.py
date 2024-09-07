920397e82e9eaeb66b095f36f6e9a970bb41a9dc
Insooooo <60099190+Insu-Park@users.noreply.github.com>
2024-09-07 11:18:27 +0900

    Time: 89 ms (19.7%), Space: 17.7 MB (32.33%) - LeetHub

--- 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.py
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.py
@@ -11,21 +11,11 @@
 #         self.right = right
 class Solution:
     def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
-        d=deque()
-        d.append(root)
-        def dfs(node1,node2):
-            if not node2:
-                return True
-            if not node1 or node1.val!=node2.val:
-                return False
-            return dfs(node1.right,node2.next) or dfs(node1.left,node2.next)
-        while(d):
-            for _ in range(len(d)):
-                curr=d.popleft()
-                if curr.val==head.val and dfs(curr,head):
-                    return True
-                if curr.left:
-                    d.append(curr.left)
-                if curr.right:
-                    d.append(curr.right)
-        return False
+        def checkPath(head, root):
+            if not head: return True
+            if not root or head.val != root.val: return False
+            return checkPath(head.next, root.left) or checkPath(head.next, root.right)
+
+        if not root: return False
+        if checkPath(head, root): return True
+        return self.isSubPath(head, root.left) or self.isSubPath(head, root.right)
